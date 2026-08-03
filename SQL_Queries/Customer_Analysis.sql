392eeed9752daf425b1ac97a2797272e2225d878
Santhiya Karuppanan <santhiya24012007@gmail.com>
2026-08-03 23:25:02 +0530

    Update Customer_Analysis.sql

--- SQL_Queries/Customer_Analysis.sql
+++ SQL_Queries/Customer_Analysis.sql
@@ -152,3 +152,66 @@ from sales s join products p
 on s.productkey=p.productkey 
 group by s.customerkey 
 order by ltv desc limit 15;
+/*=================================================================================================================================================
+                                           RECENCY,FREQUENCY AND MONETARY(RFM)
+===================================================================================================================================================*/
+with rfm as(select 
+s.customerkey,
+((select max(orderdate) from sales )-max(s.orderdate)) as recency,
+count(distinct s.ordernumber) as frequency,
+round(sum(s.orderquantity*p.productprice),2) as monetary 
+from sales s join products p 
+on s.productkey=p.productkey 
+group by s.customerkey order by recency asc,frequency desc,monetary desc),
+score as(select 
+customerkey,
+ntile(5) over(order by recency desc) as recency_score,
+ntile(5) over(order by frequency desc) as frequency_score,
+ntile(5) over(order by monetary desc) as monetary_score 
+from rfm)
+select customerkey,CONCAT(recency_score, frequency_score, monetary_score) AS rfm_score from score;
+/*=========================================================================================================================================================
+                                                      CUSTOMER DEMOGRAPHIC ANALYSIS
+===========================================================================================================================================================
+=======================================================REVENUE BY GENDER================================================================================                                                                          */
+select 
+c.gender,
+round(sum(s.orderquantity*p.productprice),2) as revenue 
+from adventureworks_customer c join sales s 
+on c.customerkey=s.customerkey join products p 
+on s.productkey=p.productkey 
+group by c.gender
+order by revenue desc;
+/*==================================================REVENUE BY OCCUPATION===================================================================================*/
+select 
+distinct c.occupation,
+round(sum(s.orderquantity*p.productprice),2) as revenue 
+from adventureworks_customer c join sales s 
+on s.customerkey=c.customerkey join products p 
+on p.productkey=s.productkey 
+group by c.occupation 
+order by revenue desc;
+/*================================================================================================================================================
+                                                WHICH EDUCATION LEVEL GENERATES THE HIGHEST REVENUE?
+==================================================================================================================================================*/
+select 
+c.educationlevel,(sum(s.orderquantity*p.productprice)/sum(s.orderquantity)) as aov,sum(s.orderquantity) as ordercount,
+round(sum(s.orderquantity*p.productprice),2) as revenue
+ from products p join sales s 
+ on p.productkey=s.productkey 
+ join adventureworks_customer c 
+ on s.customerkey=c.customerkey 
+ group by c.educationlevel
+ order by revenue desc;
+/*=================================================================================================================================================
+                                        DO RICH CUSTOMERS SPENDS THE MOST?
+====================================================================================================================================================*/
+with income_cluster as (select customerkey,ntile(5) over(order by annualincome desc) as income_clusters from adventureworks_customer)
+select 
+c.income_clusters,count(distinct c.customerkey),sum(s.orderquantity*p.productprice)/count(distinct c.customerkey) as aov,
+round(sum(s.orderquantity*p.productprice),2) as revenue 
+from income_cluster c join sales s
+ on s.customerkey=c.customerkey join products p 
+ on p.productkey=s.productkey 
+ group by c.income_clusters 
+ order by revenue desc;
